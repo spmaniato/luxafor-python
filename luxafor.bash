74bc626f73036e0705f5ad5f7e05441bf64c3c67
Spyros Maniatopoulos <spyros@mayfieldrobotics.com>
2017-10-07 16:44:14 -0700

    Add luxafor.bash script with Shell functions

---
+++ luxafor.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+LUXAFOR_DIR="/home/spyros/.luxafor-python"
+
+lux ()
+{
+  local COLOR_ARGS
+  case ${1} in
+    red)
+      COLOR_ARGS="-l 0 -r 255 -g 0 -b 0"
+      ;;
+    green)
+      COLOR_ARGS="-l 0 -r 0 -g 255 -b 0"
+      ;;
+    blue)
+      COLOR_ARGS="-l 0 -r 0 -g 0 -b 255"
+      ;;
+    white)
+      COLOR_ARGS="-l 0 -r 255 -g 255 -b 255"
+      ;;
+    off)
+      COLOR_ARGS="-l 0 -r 0 -g 0 -b 0"
+      ;;
+
+  esac
+
+  sudo ${LUXAFOR_DIR}/luxafor-linux.py color ${COLOR_ARGS}
+}
